dafdb5c3e2e8dc893574834cac74535b9a455cf2
harsharan  Singh <harsharansingh20@gmail.com>
2022-06-16 23:57:42 +0530

    greatest of 3 no's

---
+++ maxof3nos.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+
+#ifndef ONLINE JUDGE
+  freopen("input.txt","r", stdin);
+  freopen("output.txt","w", stdout);
+#endif
+
+int a,b,c;
+cin >>a>>b>>c;
+
+if (a>b)
+{
+    if (a>c)
+    {
+       cout<<"a is greatest \n";
+    }
+    else{
+    cout<<"c is greatest\n";
+}
+}
+else
+  {
+    if (b>c)
+    {
+              cout<<"b is greatest \n";
+
+    }
+    else{
+               cout<<"c is greatest \n";
+
+        }
+  }
+
+
+
+return 0;
+ }
